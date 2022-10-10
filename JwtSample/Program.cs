f7bd397619b8f1aa39d018088f61db3358eb968c
EugeneTA <tishkov2005@yandex.ru>
2022-10-10 13:31:56 +0300

    Lesson_04 Homework

---
+++ JwtSample/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace JwtSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter user name:");
+            Console.Write("> ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            Console.Write("> ");
+            string userPassword = Console.ReadLine();
+
+            UserService userService = new UserService();
+            Console.WriteLine(userService.Authenticate(userName, userPassword));
+
+            Console.ReadKey();
+        }
+    }
+}
